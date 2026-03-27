3c00b2d887c140777453895efcbfb70c072393a4
wxknss <dyrkadyrka32@mail.ru>
2026-03-27 13:55:26 +1000

    Update BotPacketListener.java

--- src/main/java/thunder/hack/features/modules/misc/BotPacketListener.java
+++ src/main/java/thunder/hack/features/modules/misc/BotPacketListener.java
@@ -2,7 +2,6 @@ package thunder.hack.features.modules.misc;
 
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.listener.ClientLoginPacketListener;
-import net.minecraft.network.packet.s2c.login.LoginCompressedS2CPacket;
 import net.minecraft.network.packet.s2c.login.LoginDisconnectS2CPacket;
 import net.minecraft.network.packet.s2c.login.LoginHelloS2CPacket;
 import net.minecraft.network.packet.s2c.login.LoginSuccessS2CPacket;
@@ -12,13 +11,33 @@ public class BotPacketListener implements ClientLoginPacketListener {
     private final BotConnection bot;
     private ClientConnection connection;
 
-    public BotPacketListener(BotConnection bot) { this.bot = bot; }
+    public BotPacketListener(BotConnection bot) {
+        this.bot = bot;
+    }
 
-    @Override public void onHello(LoginHelloS2CPacket packet) {}
-    @Override public void onSuccess(LoginSuccessS2CPacket packet) {}
-    @Override public void onDisconnect(LoginDisconnectS2CPacket packet) { bot.disconnect(); }
-    @Override public void onCompressed(LoginCompressedS2CPacket packet) {}
-    @Override public ClientConnection getConnection() { return connection; }
-    @Override public void onDisconnected(Text reason) { bot.disconnect(); }
-    @Override public boolean isConnectionOpen() { return bot.isConnected(); }
+    @Override
+    public void onHello(LoginHelloS2CPacket packet) {}
+
+    @Override
+    public void onSuccess(LoginSuccessS2CPacket packet) {}
+
+    @Override
+    public void onDisconnect(LoginDisconnectS2CPacket packet) {
+        bot.disconnect();
+    }
+
+    @Override
+    public ClientConnection getConnection() {
+        return connection;
+    }
+
+    @Override
+    public void onDisconnected(Text reason) {
+        bot.disconnect();
+    }
+
+    @Override
+    public boolean isConnectionOpen() {
+        return bot.isConnected();
+    }
 }
