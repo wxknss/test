7bdcf3786e6898a4b2758dff127a7b0f12815313
wxknss <dyrkadyrka32@mail.ru>
2026-03-27 13:58:44 +1000

    Update NameProtect.java

--- src/main/java/thunder/hack/features/modules/misc/NameProtect.java
+++ src/main/java/thunder/hack/features/modules/misc/NameProtect.java
@@ -9,73 +9,10 @@ public class NameProtect extends Module {
         super("NameProtect", Category.MISC);
     }
 
-    private final Setting<Mode> mode = new Setting<>("Mode", Mode.Default);
-    private final Setting<String> ownName = new Setting<>("OwnName", "Hell_Raider", v -> mode.is(Mode.Default) || mode.is(Mode.Obfuscated));
-    private final Setting<String> targetName = new Setting<>("TargetName", "player123", v -> mode.is(Mode.Anal));
-    private final Setting<String> fakeName = new Setting<>("FakeName", "&cFakeNick", v -> mode.is(Mode.Anal));
-    public final Setting<Boolean> hideFriends = new Setting<>("HideFriends", true);
-
-    public enum Mode {
-        Default, Anal, Obfuscated
-    }
-
     public static Setting<String> newName = new Setting<>("name", "Hell_Raider");
-    public static Setting<Boolean> hideFriendsStatic = new Setting<>("Hide friends", true);
-
-    public static String getFormattedName(net.minecraft.entity.player.PlayerEntity player) {
-        if (!ModuleManager.nameProtect.isEnabled()) {
-            return player.getDisplayName().getString();
-        }
-
-        NameProtect np = ModuleManager.nameProtect;
-        String realName = player.getGameProfile().getName();
-        boolean isSelf = player == mc.player;
-
-        switch (np.mode.getValue()) {
-            case Default:
-                if (isSelf) {
-                    return np.ownName.getValue().replace("&", "§");
-                }
-                break;
-
-            case Anal:
-                String target = np.targetName.getValue();
-                if (realName.equalsIgnoreCase(target) || player.getDisplayName().getString().contains(target)) {
-                    return np.fakeName.getValue().replace("&", "§");
-                }
-                break;
-
-            case Obfuscated:
-                if (isSelf) {
-                    String original = player.getDisplayName().getString();
-                    String colorCode = "";
-                    for (int i = 0; i < original.length() - 1; i++) {
-                        if (original.charAt(i) == '§') {
-                            colorCode = original.substring(i, i + 2);
-                            break;
-                        }
-                    }
-                    String clean = original.replaceAll("§[0-9a-fk-or]", "");
-                    return colorCode + "§k" + clean + "§r";
-                }
-                break;
-        }
-
-        return player.getDisplayName().getString();
-    }
+    public static Setting<Boolean> hideFriends = new Setting<>("Hide friends", true);
 
     public static String getCustomName() {
-        if (!ModuleManager.nameProtect.isEnabled()) {
-            return mc.getGameProfile().getName();
-        }
-        NameProtect np = ModuleManager.nameProtect;
-        if (np.mode.is(Mode.Default) || np.mode.is(Mode.Obfuscated)) {
-            return np.ownName.getValue().replace("&", "§");
-        }
-        return newName.getValue().replaceAll("&", "§");
-    }
-
-    public static boolean hideFriends() {
-        return ModuleManager.nameProtect.isEnabled() && ModuleManager.nameProtect.hideFriends.getValue();
+        return ModuleManager.nameProtect.isEnabled() ? newName.getValue().replaceAll("&", "\u00a7") : mc.getGameProfile().getName();
     }
 }
