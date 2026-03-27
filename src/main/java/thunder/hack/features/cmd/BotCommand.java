552eb31e7a6942962f7c7722fce3efdc06265109
wxknss <dyrkadyrka32@mail.ru>
2026-03-27 14:01:56 +1000

    Update BotCommand.java

--- src/main/java/thunder/hack/features/cmd/BotCommand.java
+++ src/main/java/thunder/hack/features/cmd/BotCommand.java
@@ -5,25 +5,33 @@ import net.minecraft.command.CommandSource;
 import thunder.hack.features.modules.misc.BotManager;
 
 public class BotCommand extends Command {
-    public BotCommand() { super("bot"); }
+    public BotCommand() {
+        super("bot");
+    }
 
     @Override
     public void executeBuild(LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(literal("addbot").then(argument("nick", string()).executes(ctx -> {
             String name = ctx.getArgument("nick", String.class);
-            if (BotManager.getInstance() != null) BotManager.getInstance().addBot(name);
+            if (BotManager.getInstance() != null) {
+                BotManager.getInstance().addBot(name);
+            }
             return 1;
         })));
 
         builder.then(literal("removebot").then(argument("nick", string()).executes(ctx -> {
             String name = ctx.getArgument("nick", String.class);
-            if (BotManager.getInstance() != null) BotManager.getInstance().removeBot(name);
+            if (BotManager.getInstance() != null) {
+                BotManager.getInstance().removeBot(name);
+            }
             return 1;
         })));
 
         builder.then(literal("say").then(argument("message", greedyString()).executes(ctx -> {
             String message = ctx.getArgument("message", String.class);
-            if (BotManager.getInstance() != null) BotManager.getInstance().sayAll(message);
+            if (BotManager.getInstance() != null) {
+                BotManager.getInstance().sayAll(message);
+            }
             return 1;
         })));
     }
