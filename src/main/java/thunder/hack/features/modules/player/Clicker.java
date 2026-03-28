011f1f468406d704ec8b8da29c95d0a0133498a0
wxknss <dyrkadyrka32@mail.ru>
2026-03-28 12:46:59 +1000

    Update Clicker.java

--- src/main/java/thunder/hack/features/modules/player/Clicker.java
+++ src/main/java/thunder/hack/features/modules/player/Clicker.java
@@ -5,29 +5,30 @@ import net.minecraft.block.ChestBlock;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.block.TrappedChestBlock;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.network.packet.s2c.play.OpenScreenS2CPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import thunder.hack.events.impl.PacketEvent;
 import thunder.hack.features.modules.Module;
 import thunder.hack.setting.Setting;
 import thunder.hack.utility.Timer;
-import thunder.hack.utility.player.RotationUtility;
 import thunder.hack.utility.render.Render3DEngine;
 
 import java.awt.Color;
-import java.util.Comparator;
 
-public class FunnyClicker extends Module {
-    public FunnyClicker() {
-        super("FunnyClicker", Category.PLAYER);
+public class Clicker extends Module {
+    public Clicker() {
+        super("Clicker", Category.PLAYER);
     }
 
     private final Setting<Integer> delay = new Setting<>("Delay", 50, 10, 500);
     private final Setting<Boolean> rotate = new Setting<>("Rotate", true);
+    private final Setting<Boolean> cancelGui = new Setting<>("CancelGUI", true);
     private final Setting<Boolean> render = new Setting<>("Render", true);
 
     private final Timer timer = new Timer();
@@ -36,6 +37,13 @@ public class FunnyClicker extends Module {
     @Override
     public void onEnable() {
         timer.reset();
+    }
+
+    @EventHandler
+    public void onPacketReceive(PacketEvent.Receive e) {
+        if (cancelGui.getValue() && e.getPacket() instanceof OpenScreenS2CPacket) {
+            e.cancel();
+        }
     }
 
     @Override
@@ -48,12 +56,12 @@ public class FunnyClicker extends Module {
         if (!timer.passedMs(delay.getValue())) return;
 
         if (rotate.getValue()) {
-            float[] rotations = RotationUtility.getRotations(target.getX() + 0.5, target.getY() + 0.5, target.getZ() + 0.5);
-            sendPacket(new net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.LookAndOnGround(rotations[0], rotations[1], mc.player.isOnGround()));
+            float[] rotations = getRotations(target);
+            sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(rotations[0], rotations[1], mc.player.isOnGround()));
         }
 
         BlockHitResult hit = new BlockHitResult(
-            target.toCenterPos(),
+            new Vec3d(target.getX() + 0.5, target.getY() + 0.5, target.getZ() + 0.5),
             Direction.UP,
             target,
             false
@@ -73,31 +81,36 @@ public class FunnyClicker extends Module {
     private BlockPos findNearestContainer() {
         BlockPos playerPos = mc.player.getBlockPos();
         BlockPos closest = null;
-        double closestDist = 5.0;
+        double closestDist = 36.0;
 
-        for (BlockPos pos : getNearbyBlocks(5)) {
-            Block block = mc.world.getBlockState(pos).getBlock();
-            if (block instanceof ChestBlock || block instanceof TrappedChestBlock || block instanceof ShulkerBoxBlock) {
-                double dist = playerPos.getSquaredDistance(pos);
-                if (dist < closestDist) {
-                    closestDist = dist;
-                    closest = pos;
+        for (int x = -5; x <= 5; x++) {
+            for (int y = -5; y <= 5; y++) {
+                for (int z = -5; z <= 5; z++) {
+                    BlockPos pos = playerPos.add(x, y, z);
+                    Block block = mc.world.getBlockState(pos).getBlock();
+                    if (block instanceof ChestBlock || block instanceof TrappedChestBlock || block instanceof ShulkerBoxBlock) {
+                        double dist = playerPos.getSquaredDistance(pos);
+                        if (dist < closestDist) {
+                            closestDist = dist;
+                            closest = pos;
+                        }
+                    }
                 }
             }
         }
         return closest;
     }
 
-    private java.util.List<BlockPos> getNearbyBlocks(int radius) {
-        java.util.List<BlockPos> list = new java.util.ArrayList<>();
-        BlockPos center = mc.player.getBlockPos();
-        for (int x = -radius; x <= radius; x++) {
-            for (int y = -radius; y <= radius; y++) {
-                for (int z = -radius; z <= radius; z++) {
-                    list.add(center.add(x, y, z));
-                }
-            }
-        }
-        return list;
+    private float[] getRotations(BlockPos target) {
+        Vec3d vec = new Vec3d(target.getX() + 0.5, target.getY() + 0.5, target.getZ() + 0.5);
+        Vec3d eyes = mc.player.getEyePos();
+        double diffX = vec.x - eyes.x;
+        double diffY = vec.y - eyes.y;
+        double diffZ = vec.z - eyes.z;
+
+        double yaw = Math.toDegrees(Math.atan2(diffZ, diffX)) - 90;
+        double pitch = -Math.toDegrees(Math.atan2(diffY, Math.hypot(diffX, diffZ)));
+
+        return new float[]{(float) yaw, (float) pitch};
     }
 }
