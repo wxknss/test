1a0c60d162ee7d65058eddff5a31efb8fdd1ed0d
wxknss <dyrkadyrka32@mail.ru>
2026-04-05 00:46:53 +1000

    Update ModuleManager.java

--- src/main/java/thunder/hack/core/manager/client/ModuleManager.java
+++ src/main/java/thunder/hack/core/manager/client/ModuleManager.java
@@ -140,6 +140,8 @@ public class ModuleManager implements IManager {
     public static WaterMark waterMark = new WaterMark();
     public static ViewModel viewModel = new ViewModel();
     public static TunnelEsp tunnelEsp = new TunnelEsp();
+    public static PhantomBlocks phantomBlocks = new PhantomBlocks();
+    public static EntityRide entityRide = new EntityRide();
     public static TickShift tickShift = new TickShift();
     public static TargetHud targetHud = new TargetHud();
     public static SpeedMine speedMine = new SpeedMine();
