07c3f76968db7991ffa9b361817a5bba4f5b03df
wxknss <dyrkadyrka32@mail.ru>
2026-04-21 07:18:58 +1000

    Update ModuleManager.java

--- src/main/java/thunder/hack/core/manager/client/ModuleManager.java
+++ src/main/java/thunder/hack/core/manager/client/ModuleManager.java
@@ -188,6 +188,7 @@ public class ModuleManager implements IManager {
     public static AutoBuy autoBuy = new AutoBuy();
     public static AntiFireball antiFireball = new AntiFireball();
     public static ItemDropper itemDropper = new ItemDropper();
+    public static MaceKill maceKill = new MaceKill();
     public static ArmorHud armorHud = new ArmorHud();
     public static AirPlace airPlace = new AirPlace();
     public static SelfTrap selfTrap = new SelfTrap();
