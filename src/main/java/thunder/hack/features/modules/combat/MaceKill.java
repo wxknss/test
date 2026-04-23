1eff04e20cd12908a94801049de84d59eb97d5b8
wxknss <dyrkadyrka32@mail.ru>
2026-04-24 09:37:48 +1000

    Update MaceKill.java

--- src/main/java/thunder/hack/features/modules/combat/MaceKill.java
+++ src/main/java/thunder/hack/features/modules/combat/MaceKill.java
@@ -14,97 +14,28 @@ public class MaceKill extends Module {
 
     private final Setting<Integer> fallHeight = new Setting<>("FallHeight", 22, 5, 100);
     private final Setting<Integer> teleportPackets = new Setting<>("TeleportPackets", 10, 2, 50);
-    private final Setting<Integer> fallTicks = new Setting<>("FallTicks", 8, 1, 30);
-    private final Setting<Boolean> autoDisable = new Setting<>("AutoDisable", true);
-
-    private boolean active;
-    private int tickCounter;
-    private double startY;
-    private boolean hasTeleported;
-
-    @Override
-    public void onEnable() {
-        active = false;
-        tickCounter = 0;
-        hasTeleported = false;
-    }
 
     @EventHandler
     public void onAttack(EventAttack event) {
         if (fullNullCheck()) return;
         if (mc.player.getMainHandStack().getItem() != Items.MACE) return;
-        if (active) return;
 
-        startY = mc.player.getY();
-        active = true;
-        tickCounter = 0;
-        hasTeleported = false;
-    }
+        double startX = mc.player.getX();
+        double startY = mc.player.getY();
+        double startZ = mc.player.getZ();
+        double targetY = startY + fallHeight.getValue();
 
-    @Override
-    public void onUpdate() {
-        if (!active) return;
-
-        if (!hasTeleported) {
-            double targetY = startY + fallHeight.getValue();
-
-            for (int i = 0; i < teleportPackets.getValue(); i++) {
-                sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
-                    mc.player.getX(), startY, mc.player.getZ(), true
-                ));
-            }
-
-            sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
-                mc.player.getX(), targetY, mc.player.getZ(), false
-            ));
-
-            for (int i = 0; i < 3; i++) {
-                sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
-                    mc.player.getX(), targetY, mc.player.getZ(), false
-                ));
-            }
-
-            mc.player.setPosition(mc.player.getX(), targetY, mc.player.getZ());
-            hasTeleported = true;
-            tickCounter = 0;
-            return;
+        for (int i = 0; i < teleportPackets.getValue(); i++) {
+            sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(startX, startY, startZ, true));
         }
 
-        tickCounter++;
+        mc.player.setPosition(startX, targetY, startZ);
+        sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(startX, targetY, startZ, false));
 
-        if (tickCounter < fallTicks.getValue()) {
-            double airY = startY + fallHeight.getValue();
-            sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
-                mc.player.getX(), airY, mc.player.getZ(), false
-            ));
-            mc.player.setPosition(mc.player.getX(), airY, mc.player.getZ());
-            mc.player.fallDistance = tickCounter * 5.0f;
-            return;
-        }
+        mc.player.fallDistance = fallHeight.getValue();
 
-        if (tickCounter == fallTicks.getValue()) {
-            mc.player.fallDistance = fallHeight.getValue() * 3.5f;
-
-            sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
-                mc.player.getX(), startY, mc.player.getZ(), true
-            ));
-            mc.player.setPosition(mc.player.getX(), startY, mc.player.getZ());
-            return;
-        }
-
-        if (tickCounter > fallTicks.getValue()) {
-            sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
-                mc.player.getX(), startY, mc.player.getZ(), true
-            ));
-            mc.player.setPosition(mc.player.getX(), startY, mc.player.getZ());
-            active = false;
-            hasTeleported = false;
-            tickCounter = 0;
-
-            if (autoDisable.getValue()) {
-                setEnabled(false);
-            }
-        }
+        mc.player.setPosition(startX, startY, startZ);
+        sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(startX, startY, startZ, true));
     }
 
     @Override
